d077f1946804cd1ff2ee2e1a9c3930dbb840a351
Josiah-Canada <josiahcanada@yahoo.com>
2022-05-10 17:40:30 -0400

    template updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
 const promptUser = () => {
+  const { projects, about, ...header } =templateData;
   return inquirer.prompt([
     {
       type: 'input',
@@ -126,14 +127,4 @@ Add a New Project
     });
 };
 
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
-  });
+const pageHTML = generatePage(mockData);
