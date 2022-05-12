c380db6cfb4474ebb854bea545c8574f34f1bbb4
Josiah-Canada <josiahcanada@yahoo.com>
2022-05-12 15:25:10 -0400

    final

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-const generateSite = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
-const { writeFile, copyFile } = require('./utils/generate-site.js');
-  console.log('Page created! Check out index.html in this directory to see it!');
-
-  fs.copyFile('./src/style.css', './dist/style.css', err => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log('Style sheet copied successfully!');
-  });
-});
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
-  const { projects, about, ...header } =templateData;
   return inquirer.prompt([
     {
       type: 'input',
@@ -56,25 +44,6 @@ const promptUser = () => {
     }
   ]);
 };
-
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    return generatePage(portfolioData);
-  })
-  .then(pageHTML => {
-    return writeFile(pageHTML);
-  })
-  .then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-  })
-  .then(copyFileResponse => {
-    console.log(copyFileResponse);
-  })
-  .catch(err => {
-    console.log(err);
-  });
 
 const promptProject = portfolioData => {
   console.log(`
@@ -157,4 +126,21 @@ Add a New Project
     });
 };
 
-const pageHTML = generatePage(mockData);
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
